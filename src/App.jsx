7a29b083675b6a4b0fc18e87a658315a4b7cfdcf
PARAISO NELSON <NELSONITO@air-de-paraiso.home>
2024-03-19 16:34:09 +0100

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,24 @@ import data from "./assets/data.json"
 function App() {
 
   {
+
     const dataTab = data;
+    {
+      dataTab.map((movies) => {
+        // console.log(movies);//tableau des cats et images
+        // console.log(movies.category);//category
+        //  console.log(movies.images);//tableau images
+
+        movies.images.map((pictures) => {
+          console.log(pictures);//images
+        });
+
+        return (
+          <h2>{movies.category}</h2>
+
+        )
+      })
+    };
 
     return (
       <>
@@ -12,19 +29,12 @@ function App() {
           <h1>NETFLIX</h1>
         </header>
         <main>
-          <article>
-            {
-              dataTab.map((movies) => {
-                console.log(movies);//tableau des cats et images
-                console.log(movies.category);//category
-                console.log(movies.images);//tableau images
-                return (
-                  <h2>{movies.category}</h2>
-                )
-              })
-            };
-
-          </article>
+          {data.map(category => (
+            <article>
+              <h2>{category.category}</h2>
+              {category.images}
+            </article>
+          ))};
         </main>
       </>
     )
